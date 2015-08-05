2324617e375bff8637bb4feaa58e7c5712b3a46e
Darius Vitonis <flowupon@gmail.com>
2015-08-05 11:13:52 +0200

    Renamed temp to tmp

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,16 @@ set -e
 
 apt-get install apache2-utils -y
 
-mkdir /temp -p
+mkdir /tmp -p
 
 echo Creating admin user for Kibana
-htpasswd -c /temp/.htpasswd admin
+htpasswd -c /tmp/.htpasswd admin
 echo Creating es user for Elastic Search
-htpasswd -c /temp/.espasswd es
+htpasswd -c /tmp/.espasswd es
 
 echo Starting docker containers
 docker run --name kibana-data --entrypoint /bin/echo dvitonis/elk Data-only container
 docker run -dit --name elk --volumes-from kibana-data -p 80:80 -p 9200:9201 --restart always dvitonis/elk
 
-docker run --rm --volumes-from kibana-data -v /temp:/temp dvitonis/elk cp /temp/.htpasswd /etc/nginx
-docker run --rm --volumes-from kibana-data -v /temp:/temp dvitonis/elk cp /temp/.espasswd /etc/nginx
+docker run --rm --volumes-from kibana-data -v /tmp:/tmp dvitonis/elk cp /tmp/.htpasswd /etc/nginx
+docker run --rm --volumes-from kibana-data -v /tmp:/tmp dvitonis/elk cp /tmp/.espasswd /etc/nginx
